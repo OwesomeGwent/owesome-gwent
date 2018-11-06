8ab0b46f5a25c2f0d254edb4a2aaa97a9434c6b7
Godsenal <tmqps78@gmail.com>
2018-11-07 00:45:09 +0900

    Add lerna

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Main, Sidebar } from './containers';
+import { FlipCard } from './components';
 import { ICard } from './types/card';
 
 const AppContainer = styled.div`
@@ -15,8 +16,13 @@ class App extends Component {
   async componentDidMount() {
     // 전체 카드 데이터 어디다 저장할 지 생각해봐야 할듯.
     // 일단 임시
-    const CARDS: any = await import('./data/cards.json');
-    let cardData: ICard[] = Object.keys(CARDS).map(card => CARDS[card] as ICard);
+    const [CARDS]: any = await Promise.all([
+      import('./data/cards.json'),
+      new Promise(res => setTimeout(res, 2000)),
+    ]);
+    let cardData: ICard[] = Object.keys(CARDS).map(
+      card => CARDS[card] as ICard,
+    );
     cardData = cardData.slice().sort((a: ICard, b: ICard) => {
       return a.provision - b.provision;
     });
@@ -28,7 +34,7 @@ class App extends Component {
   render() {
     const { cardData, isLoading } = this.state;
     if (isLoading) {
-      return <div>Loading cards...</div>;
+      return <FlipCard />;
     }
     return (
       <AppContainer>
---
+++ client/src/components/index.ts
@@ -0,0 +1,5 @@
+import Card from './Card';
+import CardList from './CardList';
+import FlipCard from './FlipCard';
+
+export { Card, CardList, FlipCard };

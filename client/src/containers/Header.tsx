4ff71e154e27bfa80f879a498715b1f3bb6a7e47
Kim Bum June <bjbj6363@gmail.com>
2018-11-22 00:22:15 +0900

    Fix Conflict

--- client/src/containers/Header.tsx
+++ client/src/containers/Header.tsx
@@ -1,10 +1,4 @@
 import AppBar from '@material-ui/core/AppBar';
-<<<<<<< HEAD
-import React from 'react';
-
-const Header = () => {
-  return <AppBar position="sticky">타이틀~</AppBar>;
-=======
 import Toolbar from '@material-ui/core/Toolbar';
 import React from 'react';
 import { connect } from 'react-redux';
@@ -31,6 +25,5 @@ const Header: React.SFC<IHeaderProps> = ({ locale, setLocale }) => {
       </Toolbar>
     </AppBar>
   );
->>>>>>> bbde7e1d1869cf50a16b50ea4fcfd9de98cb82da
 };
 export default Header;

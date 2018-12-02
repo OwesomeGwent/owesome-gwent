f969e700f37678923a2e3db85cf43d448703ce4d
Godsenal <tmqps78@gmail.com>
2018-12-02 19:18:59 +0900

    Fix checkOwnable

--- client/src/helpers/card.ts
+++ client/src/helpers/card.ts
@@ -2,6 +2,9 @@ import { CardData } from '../../../shared/ICardData';
 
 // Ownable 검사. 덱에 못넣는 카드들.
 export const checkOwnable = (card: CardData) => {
+  if (!card.variations) {
+    return false;
+  }
   return card.variations[0].availability !== 'NonOwnable';
 };
 export const sortByFaction = (a: CardData, b: CardData) => {

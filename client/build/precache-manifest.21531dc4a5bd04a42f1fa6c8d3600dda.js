39287295b32e2bff9c45fa75764e906df5387cad
Godsenal <tmqps78@gmail.com>
2018-12-09 21:58:58 +0900

    Add build

---
+++ client/build/precache-manifest.21531dc4a5bd04a42f1fa6c8d3600dda.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a1e753f2a81fc0712f",
+    "url": "/static/css/main.eaeaa86a.chunk.css"
+  },
+  {
+    "revision": "b6a1e753f2a81fc0712f",
+    "url": "/static/js/main.b6a1e753.chunk.js"
+  },
+  {
+    "revision": "4b891c4a829745e038cc",
+    "url": "/static/js/1.4b891c4a.chunk.js"
+  },
+  {
+    "revision": "08a23a0a2f00cacf855d",
+    "url": "/static/js/2.08a23a0a.chunk.js"
+  },
+  {
+    "revision": "f211b82e3697cebac2f7",
+    "url": "/static/js/runtime~main.f211b82e.js"
+  },
+  {
+    "revision": "8d16403141d99562d230a68808fe2e63",
+    "url": "/static/media/card.8d164031.png"
+  },
+  {
+    "revision": "137828bfe0a033d96633def254695dae",
+    "url": "/static/media/provision.137828bf.png"
+  },
+  {
+    "revision": "efef99f7e03db373e79da21b69fb1d1c",
+    "url": "/static/media/12950000.efef99f7.png"
+  },
+  {
+    "revision": "b063264537843efcc1964bcba7e5eacd",
+    "url": "/static/media/craft.b0632645.png"
+  },
+  {
+    "revision": "b99f7bf6ddefd1f0942aabc91b736571",
+    "url": "/static/media/frame-basis-gold.b99f7bf6.png"
+  },
+  {
+    "revision": "2551a6345f0779addabc537090518f68",
+    "url": "/static/media/back.2551a634.png"
+  },
+  {
+    "revision": "0a79f478ae77506363c9d672eec05b87",
+    "url": "/static/media/banner-nil-basic.0a79f478.png"
+  },
+  {
+    "revision": "f965728770d3677cf8a7d9c2dd8d3db2",
+    "url": "/static/media/banner-mon-basic.f9657287.png"
+  },
+  {
+    "revision": "0dc06c951e3049bb0ad4a8adfd3a4eb9",
+    "url": "/static/media/banner-nor.0dc06c95.png"
+  },
+  {
+    "revision": "81a07063431c148924fca0698f446917",
+    "url": "/static/media/banner-sco.81a07063.png"
+  },
+  {
+    "revision": "a7d6fc3e2b3307bdd7c4a7a7a38d4cc6",
+    "url": "/static/media/icon-artifact.a7d6fc3e.png"
+  },
+  {
+    "revision": "92602208a82c6934182977261b8006dc",
+    "url": "/static/media/icon-leader.92602208.png"
+  },
+  {
+    "revision": "3ae2237635bf1cb5bea5134459f77804",
+    "url": "/static/media/provision-icon.3ae22376.png"
+  },
+  {
+    "revision": "aa091e93987e2594428ff85098689950",
+    "url": "/static/media/icon-spell.aa091e93.png"
+  },
+  {
+    "revision": "7e8027e90ffaf4d04cf3f5c5dae34931",
+    "url": "/static/media/banner-ske.7e8027e9.png"
+  },
+  {
+    "revision": "f562e5786e260196e9925e8a27f91aad",
+    "url": "/static/media/leader-mulligan.f562e578.png"
+  },
+  {
+    "revision": "b79edb53f0868956a3966eb2c25ddc9e",
+    "url": "/static/media/banner-neu.b79edb53.png"
+  },
+  {
+    "revision": "7a65af594d0d4174ff99e7e8a64c3022",
+    "url": "/static/media/banner-mon-leader.7a65af59.png"
+  },
+  {
+    "revision": "55fe3a640730dfacce19f354af2bf410",
+    "url": "/static/media/banner-nil-leader.55fe3a64.png"
+  },
+  {
+    "revision": "6ab96970c54ba7a68143f70446acab34",
+    "url": "/static/media/banner-ske-leader.6ab96970.png"
+  },
+  {
+    "revision": "7323d884f8c11aa1e7ed4758809aace0",
+    "url": "/static/media/banner-nor-leader.7323d884.png"
+  },
+  {
+    "revision": "4e88f4933a69e7365327557881385d22",
+    "url": "/static/media/banner-sco-leader.4e88f493.png"
+  },
+  {
+    "revision": "a278e74a6e8c32d1ea53c5e5b8a2027d",
+    "url": "/static/media/frame-basis-bronze.a278e74a.png"
+  },
+  {
+    "revision": "2777c513f60da58078feaf222a194ef9",
+    "url": "/static/media/frame-basis-silver.2777c513.png"
+  },
+  {
+    "revision": "4242140e17882e02a46ff22049d53498",
+    "url": "/static/media/frame-basis-leader.4242140e.png"
+  },
+  {
+    "revision": "1c34b2fc26a132879e7c1f7153e48228",
+    "url": "/static/media/provision-mon.1c34b2fc.png"
+  },
+  {
+    "revision": "ca3ee5990066844db4136c575466921e",
+    "url": "/static/media/provision-nor.ca3ee599.png"
+  },
+  {
+    "revision": "21907683d80c858dbbf85443ce175aec",
+    "url": "/static/media/provision-neu.21907683.png"
+  },
+  {
+    "revision": "d067deec7bd95e5cfab4a75666d57b66",
+    "url": "/static/media/provision-ske.d067deec.png"
+  },
+  {
+    "revision": "ed42e7894f8dc84f4ffbcfe2d9b2e13d",
+    "url": "/static/media/provision-nil.ed42e789.png"
+  },
+  {
+    "revision": "5d396221709b2a1d8aaa12c12572e436",
+    "url": "/static/media/provision-sco.5d396221.png"
+  },
+  {
+    "revision": "934374279afbb9e3f816105eb769c93f",
+    "url": "/index.html"
+  }
+];
